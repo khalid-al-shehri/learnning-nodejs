e42239054addd1eb0fecdc9d65f345cf75c850aa
Khalid <kalshehri@jahez.net>
2022-05-29 09:27:56 +0300

    48. Finding & Fixing Syntax Errors 49. Dealing with Runtime Errors 50. Logical Errors 51. Using the Debugger

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -1,9 +1,5 @@
 // Create server
 const http = require('http');
-
-// Test nodemon
-console.log("NODEMON is here");
-
 const routes = require('./routes');
 const server = http.createServer(routes.handler);
 server.listen(3000);
