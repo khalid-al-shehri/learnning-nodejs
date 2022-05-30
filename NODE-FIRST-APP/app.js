6124e908e6b2b257033aacd337e4ac9cfb988d02
Khalid <kalshehri@jahez.net>
2022-05-30 09:58:33 +0300

    63. Handling Different Routes

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -4,15 +4,20 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-// next is function to go NEXT app use (Next Middleware).
-app.use((req, res, next) => {
-    console.log("Middleware 1");
+app.use("/", (req, res, next) => {
+    console.log("This print always runs!");
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("Middleware 2");
-    res.send("Hi Welcome");
+// next is function to go NEXT app use (Next Middleware).
+app.use("/add-product", (req, res, next) => {
+    console.log("add-produc page");
+    res.send("Hi Welcome to add-product page");
+});
+
+app.use("/", (req, res, next) => {
+    console.log("home page");
+    res.send("Hi Welcome to home page");
 });
 
 const server = http.createServer(app);
