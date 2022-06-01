70a5899dd67dc5ba700a446eda6381f6f0c2811e
Khalid <kalshehri@jahez.net>
2022-06-01 09:08:48 +0300

    79. Sharing Data Across Requests & Users

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -6,19 +6,19 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 // Body-parser
 app.use(bodyParser.urlencoded({extended: false}));
-// Serving Files Statically
+// Serving Files Staticallt
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 // 404 Page
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
